0ca846145723b3ace38e3c8d0040801a5f62cb47
Bruno Galvao <brunopgalvao@gmail.com>
2019-12-17 17:21:12 -0500

    remove sidebar text

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 import './Sidebar.css'
 
-const Sidebar = ({ closed }) => <div className={closed ? 'sidebar-container closed' : 'sidebar-container'}>Sidebar</div>
+const Sidebar = ({ closed }) => <div className={closed ? 'sidebar-container closed' : 'sidebar-container'}></div>
 
 export default memo(Sidebar)
